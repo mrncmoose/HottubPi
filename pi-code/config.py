36879b2b087e36ea13a0d9dc8711c15e7ba976de
mrncmoose <mrncmoose@gmail.com>
2021-05-26 16:59:19 -0400

    Filtering functions added

--- pi-code/config.py
+++ pi-code/config.py
@@ -6,11 +6,13 @@ config = {
         "light": 10
     },
     "limits": {
-        "max_temp": 110,
-        "min_temp": 34,
-        "filter_hours": 5,
-        "max_at_temp_hours": 2,
+        "max_temp": 30,
+        "min_temp": 5,
+        "max_at_temp_hours": 1,
     },
     "filter_on": '09:30:00',
-    'filter_off': '13:30:00',
+    'filter_off': '12:30:00',
+    'temp_sensor_path': ' /sys/bus/w1/devices/28-0118410baaff/w1_slave',
+    'temp_cal_m': 1.0,
+    'temp_cal_b': 0.0,
 }
--- pi-code/maine.py
+++ pi-code/maine.py
@@ -11,8 +11,10 @@ import RPi.GPIO as GPIO
 import argparse
 import logging
 import logging.handlers
+import threading
+import signal
 
-from config import config
+from Config import config
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--log_level", 
@@ -32,4 +34,112 @@ logFormatter = logging.Formatter('%(levelname)s\t%(asctime)s\t%(message)s')
 logHandler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=20000000, backupCount=2 )
 logHandler.setFormatter(logFormatter)
 eventLogger.addHandler(logHandler)
-startTime = datetime.datetime.now()
+
+class Controller():
+    def __init__(self):
+        self.mode = "STANDBY"
+        self.temp_sensor_path = config['temp_sensor_path']
+        self.temp_cal_m = config['temp_cal_m']
+        self.temp_cal_b = config['temp_cal_b']
+        self.temp_setpoint = config['limits'].['min_temp']
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        # Set relay pins as output
+        GPIO.setup(config['GPIO']['heat'], GPIO.OUT)
+        GPIO.setup(config['GPIO']['pump_low'], GPIO.OUT)
+        GPIO.setup(config['GPIO']['pump_high'], GPIO.OUT)
+        GPIO.setup(config['GPIO']['light'], GPIO.OUT)
+        #initialize to off
+        GPIO.output(config['GPIO']['heat'], GPIO.LOW)
+        GPIO.output(config['GPIO']['pump_low'], GPIO.LOW)
+        GPIO.output(config['GPIO']['pump_high'], GPIO.LOW)
+        GPIO.output(config['GPIO']['light'], GPIO.LOW)
+
+    def getCurrentTemp(self):
+        # append the device file name to get the absolute path of the sensor 
+        fileobj = open(self.self.temp_sensor_path,'r')
+        lines = fileobj.readlines()
+        fileobj.close()
+        myRegex = re.compile(r"=")
+        crc = myRegex.split(lines[0])
+        crc = crc[1][3:-1]
+        tempStr = myRegex.split(lines[1])
+        tempVal = round(float(tempStr[1])/1000, 1)
+        tempRetVal = 30
+        if "YES" in crc:
+            eventLogger.info("Temperature\t " + str(tempVal))
+            tempRetVal = (tempVal * self.temp_cal_m) + self.temp_cal_b
+        else:
+            eventLogger.warn("Got bad crc reading temperature sensor")
+        return tempRetVal
+
+    def filtering_on(self):
+        if self.mode != "HEATING":
+            self.mode = "FILTERING"
+            self.set_pump_level('LOW')
+
+    def filtering_off(self):
+        if self.mode != "HEATING":
+            self.mode = "STANDBY"
+            self.set_pump_level('OFF')
+
+    def heating(self, setpoint):
+        self.mode = "HEATING"
+        GPIO.output(config['GPIO']['heat'], GPIO.HIGH)
+        self.set_pump_level('LOW')
+        if setpoint <= config['limits']['max_temp']:
+            self.temp_setpoint = setpoint
+        else:
+            self.temp_setpoint = config['limits']['max_temp']
+        return self.hold_temp()
+
+    def set_pump_level(self, level):
+        if level == 'OFF':
+            GPIO.output(config['GPIO']['pump_low'], GPIO.LOW)
+            GPIO.output(config['GPIO']['pump_high'], GPIO.LOW)
+        if level == 'HIGH':
+            GPIO.output(config['GPIO']['pump_low'], GPIO.LOW)
+            GPIO.output(config['GPIO']['pump_high'], GPIO.HIGH)
+        if level == 'LOW':
+            GPIO.output(config['GPIO']['pump_low'], GPIO.HIGH)
+            GPIO.output(config['GPIO']['pump_high'], GPIO.LOW)       
+
+    def hold_temp(self):
+        currentTemp = self.getCurrentTemp()
+        if currentTemp > self.temp_setpoint + 1.0:
+            GPIO.output(config['GPIO']['heat'], GPIO.LOW) 
+        if currentTemp <= self.temp_setpoint -0.5:
+            GPIO.output(config['GPIO']['heat'], GPIO.HIGH)
+
+        return currentTemp
+
+##----------------- End of class
+
+def exit_clean(signum, frame):
+    print('Exiting on kill signal')
+    GPIO.cleanup()
+    sys.exit(0)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, exit_clean)
+    signal.signal(signal.SIGABRT, exit_clean)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log_level", 
+                    help="The level of log messages to log", 
+                    default="INFO", 
+                    choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'])
+    args = parser.parse_args()
+    controller = Controller()
+
+    while True:
+        now =  time.localtime()
+        filterOn = config['filter_on'].split(':')
+        filterOff = config['filter_off'].split(':')
+        if filterOn[0] == now.tm_hour and filterOn[1] == now.tm_min:
+            controller.filtering_on()
+        if filterOff[0] == now.tm_hour and filterOff[1] == now.tm_min:
+            controller.filtering_off()
+
+        time.sleep(1)
+        
